d2a64bc44866e704bd7707f9261dffc1c68fca54
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-07-09 10:41:41 +0200

    Add project files.

---
+++ ApprenticeshipApp.Domain/Entities/Course.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApprenticeshipApp.Domain.Entities
+{
+    public class Course
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Only positive number allowed")]
+        public int DurationInHours { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public Guid InstructorId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public User Instructor { get; set; }
+        public ICollection<Session> Sessions { get; set; }
+    }
+}
---
+++ ApprenticeshipApp.Domain/Entities/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApprenticeshipApp.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(200)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string Role { get; set; } = "User"; // Default role is User
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
