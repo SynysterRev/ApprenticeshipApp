4808245e0cee0f00bce41c23a2e8e3008e7fb44a
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-07-25 16:43:30 +0200

    start making header

--- client/JuniorOnly.ClientApp/src/app/app.ts
+++ client/JuniorOnly.ClientApp/src/app/app.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Company } from './services/company';
 import { OfferCard } from './domains/offer/components/offer-card/offer-card';
+import { Header } from './core/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, OfferCard],
+  imports: [RouterOutlet, OfferCard, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
-  constructor(private companyService: Company) {}
+  constructor(private companyService: Company) { }
 
   ngOnInit(): void {
     // this.companyService.getCompanies().subscribe({
