190eb8796049a1eda2d7aa6309f74183e2cb391f
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-08-04 09:22:25 +0200

    add router link to some links in footer

--- client/JuniorOnly.ClientApp/src/app/core/layout/footer/footer.ts
+++ client/JuniorOnly.ClientApp/src/app/core/layout/footer/footer.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { LucideAngularModule, Briefcase } from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule,
+    RouterLink
+  ],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
